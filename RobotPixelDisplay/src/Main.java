278b914243f7c4dbf008c1eb622726997d81c784
Jimmie <Jimmie@DESKTOP-EO7C3V7.lan>
2017-01-25 21:27:10 -0500

    Increased Speed

--- RobotPixelDisplay/src/Main.java
+++ RobotPixelDisplay/src/Main.java
@@ -75,7 +75,7 @@ public class Main {
 		    jf.pack();
 		    jf.setVisible(true);
 		    try {
-				TimeUnit.MILLISECONDS.sleep(10);
+				TimeUnit.MILLISECONDS.sleep(1);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
