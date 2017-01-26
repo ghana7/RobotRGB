a7b22eb03d64a1a3d28702c202b601b3045420a2
Jimmie <Jimmie@DESKTOP-EO7C3V7.lan>
2017-01-26 18:04:18 -0500

    added sparkle, spiral, started bounce

--- RobotPixelDisplay/src/Main.java
+++ RobotPixelDisplay/src/Main.java
@@ -2,86 +2,117 @@ import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.concurrent.TimeUnit;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
-//this is a test
+
 public class Main {
-	public static Image getImageFromArray(int[] pixels, int width, int height)
-	  {
-	    BufferedImage image =
-	        new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-	    WritableRaster raster = (WritableRaster) image.getData();
-	    raster.setPixels(0, 0, width, height, pixels);
-	    image.setData(raster);
-	    return image;
-	  }
-	public static Image getImageFromArray(Pixel[][] pixels, int width, int height) {
-		BufferedImage image =
-		        new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+	public static Image getImageFromArray(int[] pixels, int width, int height) {
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		WritableRaster raster = (WritableRaster) image.getData();
-		for(int i = 0; i < height; i++) {
-			for(int j = 0; j < width; j++) {
+		raster.setPixels(0, 0, width, height, pixels);
+		image.setData(raster);
+		return image;
+	}
+
+	public static Image getImageFromArray(Pixel[][] pixels, int width, int height) {
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+		WritableRaster raster = (WritableRaster) image.getData();
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
 				raster.setPixel(j, i, pixels[i][j].getColorArray());
 			}
 		}
 		image.setData(raster);
-	    return image;
+		return image;
 	}
+
 	public static Image getPixelatedImageFromArray(Pixel[][] pixels, int width, int height) {
-		BufferedImage image =
-		        new BufferedImage(width * 16, height * 16, BufferedImage.TYPE_INT_RGB);
+		BufferedImage image = new BufferedImage(width * 16, height * 16, BufferedImage.TYPE_INT_RGB);
 		WritableRaster raster = (WritableRaster) image.getData();
-		for(int i = 0; i < height * 16; i = i + 16) {
-			for(int j = 0; j < width * 16; j = j + 16) {
-				for(int modI = 0; modI < 16; modI++) {
-					for(int modJ = 0; modJ < 16; modJ++) {
-						raster.setPixel(j + modJ, i + modI, pixels[i/16][j/16].getColorArray());
+		for (int i = 0; i < height * 16; i = i + 16) {
+			for (int j = 0; j < width * 16; j = j + 16) {
+				for (int modI = 0; modI < 16; modI++) {
+					for (int modJ = 0; modJ < 16; modJ++) {
+						raster.setPixel(j + modJ, i + modI, pixels[i / 16][j / 16].getColorArray());
 					}
 				}
 			}
 		}
 		image.setData(raster);
-	    return image;
+		return image;
 	}
-	  public static void main(String[] args) throws IOException
-	  {
-	    JFrame jf = new JFrame();
-	    JLabel jl = new JLabel();
-	    int tick = 0;
-	    int WIDTH = 18;
-	    int HEIGHT = 18;
-	    
-	    Pixel[][] arrayimage = new Pixel[HEIGHT][WIDTH];
-	    for(int i = 0; i < HEIGHT; i++) {
-	    	for(int j = 0; j < WIDTH; j++) {
-	    		arrayimage[i][j] = new Pixel(0,0,0);
-	    	}
-	    }
-	    Pixel pix = new Pixel(0,0,0);
-	    while(true) {
-	    	tick = (++tick) % 648;
-	    	
-	    	for (int i = 0; i < HEIGHT; i++)
-		    {
-	    		for (int j = 0; j < WIDTH; j++) {
-	    			//arrayimage[i][j].pulsateCircular(tick,i,j);
-	    			arrayimage[i][j].pulsateDiagonal(tick, i, j);
-	    			//arrayimage[i][j].randomNoise();
-	    		}
-		    }
-		    ImageIcon ii = new ImageIcon(getPixelatedImageFromArray(arrayimage, WIDTH, HEIGHT));
-		    jl.setIcon(ii);
-		    jf.add(jl);
-		    jf.pack();
-		    jf.setVisible(true);
-		    try {
+	public static void moveBounce(int bounceDir, int bounceX, int bounceY) {
+		//0 - top left, 1 - top right, 2 - bottom right, 3 - bottom left
+		if(bounceX == 0) {
+			bounceDir = 3 - bounceDir;
+		}
+		if(bounceX == 17) {
+			bounceDir = 3 - bounceDir;
+		}
+		if(bounceY == 0) {
+			bounceDir = 1 - bounceDir;
+		}
+		if(bounceY == 17) {
+			bounceDir = 5 - bounceDir;
+		}
+		switch(bounceDir) {
+		case 0:
+			bounceX--;
+			bounceY--;
+			break;
+		case 1:
+			bounceX++;
+			bounceY--;
+			break;
+		case 2:
+			bounceX++;
+			bounceY++;
+			break;
+		case 3:
+			bounceX--;
+			bounceY++;
+			break;
+		}
+	}
+	public static void main(String[] args) throws IOException {
+		JFrame jf = new JFrame();
+		JLabel jl = new JLabel();
+		int tick = 0;
+		int WIDTH = 18;
+		int HEIGHT = 18;
+		int bounceDir = 0;
+		int bounceX = 5;
+		int bounceY = 10;
+
+		Pixel[][] arrayimage = new Pixel[HEIGHT][WIDTH];
+		for (int i = 0; i < HEIGHT; i++) {
+			for (int j = 0; j < WIDTH; j++) {
+				arrayimage[i][j] = new Pixel(0, 0, 0);
+			}
+		}
+		Pixel pix = new Pixel(0, 0, 0);
+		while (true) {
+			tick = (++tick) % 648;
+			moveBounce(bounceDir,bounceX,bounceY);
+			for (int i = 0; i < HEIGHT; i++) {
+				for (int j = 0; j < WIDTH; j++) {
+					// arrayimage[i][j].pulsateCircular(tick,i,j);
+					// arrayimage[i][j].pulsateDiagonal(tick,i,j);
+					// arrayimage[i][j].randomNoise();
+					// arrayimage[i][j].movingSpiral(tick,i,j,20);
+					// arrayimage[i][j].sparkle(0.00002);
+					arrayimage[i][j].drawColor(i,j,bounceX,bounceY);
+				}
+			}
+			ImageIcon ii = new ImageIcon(getPixelatedImageFromArray(arrayimage, WIDTH, HEIGHT));
+			jl.setIcon(ii);
+			jf.add(jl);
+			jf.pack();
+			jf.setVisible(true);
+			try {
 				TimeUnit.MILLISECONDS.sleep(1);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-	    }
-	    
-	  }
+		}
+	}
 }
--- RobotPixelDisplay/src/Pixel.java
+++ RobotPixelDisplay/src/Pixel.java
@@ -1,24 +1,20 @@
-import java.awt.Color;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-
 public class Pixel {
 	private int red;
 	private int green;
 	private int blue;
+
 	public Pixel(int _red, int _green, int _blue) {
 		red = _red;
 		green = _green;
 		blue = _blue;
 	}
+
 	public Pixel(Pixel p) {
 		red = p.red;
 		green = p.green;
 		blue = p.blue;
 	}
+
 	public int[] getColorArray() {
 		int[] out = new int[3];
 		out[0] = red;
@@ -26,6 +22,7 @@ public class Pixel {
 		out[2] = blue;
 		return out;
 	}
+
 	public String toString() {
 		String output = "";
 		output += "R:" + red;
@@ -33,12 +30,14 @@ public class Pixel {
 		output += " B:" + blue;
 		return output;
 	}
+
 	public void setColor(int input) {
 		double frequency = Math.PI / 324;
-		red = 128 + (int)(127 * Math.sin(frequency * input));
-		blue = 128 + (int)(127 * Math.sin(frequency * input + frequency * 216));
-		green = 128 + (int)(127 * Math.sin(frequency * input + frequency * 432));
+		red = 128 + (int) (127 * Math.sin(frequency * input));
+		blue = 128 + (int) (127 * Math.sin(frequency * input + frequency * 216));
+		green = 128 + (int) (127 * Math.sin(frequency * input + frequency * 432));
 	}
+
 	public void setColor(int _red, int _green, int _blue) {
 		red = _red;
 		green = _green;
@@ -46,25 +45,57 @@ public class Pixel {
 	}
 	public void toGrayScale() {
 		int sum = red + green + blue;
-		red = sum/3;
-		green = sum/3;
-		blue = sum/3;
+		red = sum / 3;
+		green = sum / 3;
+		blue = sum / 3;
 	}
+
 	public void pulsateCircular(int tick, int x, int y) {
-		setColor(648 - tick + 16 * (int)(1 + Math.abs(x - 8.5) + Math.abs(y - 8.5)));
+		setColor(648 - tick + 16 * (int) (1 + Math.abs(x - 8.5) + Math.abs(y - 8.5)));
 	}
+
 	public void pulsateDiagonal(int tick, int x, int y) {
 		setColor(tick + 16 * x + 16 * y);
 	}
-	public void randomNoise() {
-		setColor((int)(Math.random() * 256),(int)(Math.random() * 256),(int)(Math.random() * 256));
+	public void spiral(int tick, int x, int y, double cenX, double cenY) {
+		double dX = x - cenX;
+		double dY = y - cenY;
+		double angle = Math.atan2(dY, dX);
+		angle += Math.PI;
+		angle *= (648 / (Math.PI * 2));
+		setColor(tick + (int)angle);
 	}
-	public void showImage() {
-		BufferedImage img = null;
-		try {
-		    img = ImageIO.read(new File("strawberry.jpg"));
-		} catch (IOException e) {
-		}
+	public void movingSpiral(int tick, int x, int y, int radius) {
+		double cenAngle = tick;
+		cenAngle *= (Math.PI / 324);
+		cenAngle -= Math.PI;
+		double cenX = Math.cos(cenAngle) * radius + 8.5;
+		double cenY = Math.sin(cenAngle) * radius + 8.5;
 		
+		double dX = x - cenX;
+		double dY = y - cenY;
+		double angle = Math.atan2(dY, dX);
+		angle += Math.PI;
+		angle *= (648 / (Math.PI * 2));
+		setColor(tick + (int)angle);
+	}
+	public void sparkle(double frequency) {
+		if(getColorArray()[0] == 0 && getColorArray()[1] == 0 && getColorArray()[2] == 0) {
+			if(Math.random() < frequency) {
+				setColor(255,255,255);
+			}
+		} else {
+			setColor((int)(getColorArray()[0] * 0.99), (int)(getColorArray()[1] * 0.99), (int)(getColorArray()[2] * 0.99));
+		}
+	}
+	public void drawColor(int x, int y, int ballx, int bally) {
+		if(x == ballx && y == bally) {
+			setColor(255,255,255);
+		} else {
+			setColor(0,0,0);
+		}
+	}
+	public void randomNoise() {
+		setColor((int) (Math.random() * 256), (int) (Math.random() * 256), (int) (Math.random() * 256));
 	}
 }
